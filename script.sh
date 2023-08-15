22ac0ce496b2663d7ecc6bf6786013e2773f33e7
Heinrich Klink <Heinrich.Klink@docc.techstarter.de>
2023-08-15 14:32:34 +0200

    Ich sehe was, was du nicht siehst

--- script.sh
+++ script.sh
@@ -3,9 +3,15 @@
 echo "Willkommen bei WSL"
 
 bucket=dasistunscherbugget
+cidr=15.0.0.0/24
 
-aws s3 mb s3://$bucket
-aws s3 ls 
-aws s3 rb s3://$bucket
+vpc_id=$(aws ec2 create-vpc --cidr-block $cidr --query Vpc.VpcId --output text)
+read -n1 -p "weiter"
+aws ec2 delete-vpc --vpc-id $vpc_id
+
+
+#aws s3 mb s3://$bucket
+#aws s3 ls 
+#aws s3 rb s3://$bucket
 
 
