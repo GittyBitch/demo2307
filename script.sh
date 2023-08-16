3b7ebef44f6108b237df9525839ff72812243d56
Heinrich Klink <Heinrich.Klink@docc.techstarter.de>
2023-08-16 15:41:01 +0200

    Variablen und Subnet hinzugefügt

--- script.sh
+++ script.sh
@@ -6,8 +6,9 @@ bucket=dasistunscherbugget
 cidr=15.0.0.0/24
 
 vpc_id=$(aws ec2 create-vpc --cidr-block $cidr --query Vpc.VpcId --output text)
-read -n1 -p "weiter"
-aws ec2 delete-vpc --vpc-id $vpc_id
+subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block $cidr --query Subnet.SubnetId --output text)
+read -n1 -p "weiter ($subnet_id)"
+aws ec2 delete-subnet --subnet-id $subnet_id && aws ec2 delete-vpc --vpc-id $vpc_id
 
 
 #aws s3 mb s3://$bucket
