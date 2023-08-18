2b3035cce2837eb574eb8270caa8d1c311d87071
Boris <bobby@TP2023>
2023-08-18 10:43:50 +0200

    Revert "Verbesserung"
    This reverts commit 693021ccff8cb3c74dc8f50d146661ee61f09689.

--- PythonDemo.py
+++ PythonDemo.py
@@ -1,2 +1,18 @@
-IHR SEID DOCH ALLE DUMME ÄRSCHE !!!!!!!!!!!!!
+print ("Willkommen")
+Liste = ["Hamburg","Köln",3,4,5]
+print ( Liste[1] )
+
+
+Person = {
+
+        "name": "Karl-Heinz",
+        "postleitzahl": "11111",
+        "strasse": "weg 8",
+        "sexpartner": ["Bernd", "Adolf", "Benny", "der andere Bernd"],
+}
+
+print ( Person["postleitzahl"] ) 
+print ( Person["sexpartner"] [0]) 
+print ("adios")
+
 
