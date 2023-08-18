69fb7de34d9b6b9dec34071d5de9dcd1fc7d21c4
Boris <bobby@TP2023>
2023-08-18 14:29:50 +0200

    Personendaten aus einer separaten JSON-Datei lesen

--- PythonDemo.py
+++ PythonDemo.py
@@ -5,17 +5,9 @@ print ("Willkommen")
 Liste = ["Hamburg","Köln",3,4,5]
 print ( Liste[1] )
 
-Person = {
-
-        "name": "Karl-Heinz",
-        "postleitzahl": "11111",
-        "strasse": "weg 8",
-        "sexpartner": ["Bernd", "Adolf", "Benny", "der andere Bernd"],
-}
-
 dateiname = "Infos.json"
-with open(dateiname, "w") as write_file:
-    json.dump(Person, write_file, indent=4)
+with open(dateiname, "r") as read_file:
+    Person=json.load(read_file)
 
 print ( Person["postleitzahl"] ) 
 print ( Person["sexpartner"] [0]) 
