693021ccff8cb3c74dc8f50d146661ee61f09689
Boris <bobby@TP2023>
2023-08-18 10:22:40 +0200

    Verbesserung

--- PythonDemo.py
+++ PythonDemo.py
@@ -1,18 +1,2 @@
-print ("Willkommen")
-Liste = ["Hamburg","Köln",3,4,5]
-print ( Liste[1] )
-
-
-Person = {
-
-        "name": "Karl-Heinz",
-        "postleitzahl": "11111",
-        "strasse": "weg 8",
-        "sexpartner": ["Bernd", "Adolf", "Benny", "der andere Bernd"],
-}
-
-print ( Person["postleitzahl"] ) 
-print ( Person["sexpartner"] [0]) 
-print ("adios")
-
+IHR SEID DOCH ALLE DUMME ÄRSCHE !!!!!!!!!!!!!
 
