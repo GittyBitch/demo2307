bcb5e1fa7e0a1c822f08148ea2c5bf729a07643f
Boris <bobby@TP2023>
2023-08-18 12:27:06 +0200

    JSON Daten in separater Datei speichern

--- PythonDemo.py
+++ PythonDemo.py
@@ -1,7 +1,9 @@
+# ein Python module importieren um json Dateien zu bearbeiten
+import json
+
 print ("Willkommen")
 Liste = ["Hamburg","Köln",3,4,5]
 print ( Liste[1] )
-
 
 Person = {
 
@@ -10,6 +12,10 @@ Person = {
         "strasse": "weg 8",
         "sexpartner": ["Bernd", "Adolf", "Benny", "der andere Bernd"],
 }
+
+dateiname = "Infos.json"
+with open(dateiname, "w") as write_file:
+    json.dump(Person, write_file, indent=4)
 
 print ( Person["postleitzahl"] ) 
 print ( Person["sexpartner"] [0]) 
